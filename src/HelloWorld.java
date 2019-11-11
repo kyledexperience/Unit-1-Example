975508b491b4d6ed0b2d5c959c8a7d78064216a1
kyledexperience <kyledexperience@gmail.com>
2019-11-11 13:37:20 -0500

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author kyledixon
+ *
+ */
+
+/*
+ * this is an example of a Java class
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+	}
+
+}
